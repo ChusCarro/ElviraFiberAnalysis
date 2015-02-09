6b87c2a6ef9a68dc6e020a8e04290d699dbf5f72
Jesús Carro <chuscarro@gmail.com>
2015-02-09 14:04:51 +0100

    Solved bug

--- source/createFileNodes.m
+++ source/createFileNodes.m
@@ -3,8 +3,8 @@ function createFileNodes(pathToSave, nodes)
 f=fopen([pathToSave '/data/file_nodes.dat'],'w');
 
 fprintf(f,'!----------- FILE NODES -------------------------------\n');
-fprintf(f,[num2str(length(nodes)) ' 1 1\n');
-fprintf(f,[num2str(length(nodes)) ' NODETYPE\n');
+fprintf(f,[num2str(length(nodes)) ' 1 1\n']);
+fprintf(f,[num2str(length(nodes)) ' NODETYPE\n']);
 for i=1:length(nodes)
   fprintf(f,['  ' num2str(i) ' 1 ' num2str(nodes(i)) '\n'])
 end 
