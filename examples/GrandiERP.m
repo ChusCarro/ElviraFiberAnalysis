9d02d8037f11268f3671406e4cf5d1940dbf5ea4
Jesús Carro <chuscarro@gmail.com>
2015-02-09 17:45:21 +0100

    Example Grandi

--- examples/GrandiERP.m
+++ examples/GrandiERP.m
@@ -2,6 +2,7 @@ addpath('../source')
 
 pathToSave = '~/FiberResults/GrandiEstable0001';
 mainElvira = '~/Software/Elvira/Elvira20150121/bin/mainelv_infiniband_gcc';
+project = 'Hperkalemia in fibre - Grandi Model';
 cellType = 13;
 K=[4:0.1:11];
 cores=4;
@@ -19,10 +20,19 @@ Istep = 1;
 CI_step = 0.1;
 sigma_L = 0.0013;
 Cm = 1;
-nodes = [0:0.01:4];
+L = 4;
+dx = 0.01;
+nOut = 5;
+dxOut = 0.25;
+nodes = [0:dx:L];
 
-calculateERP(cores, pathToSave, mainElvira, cellType, K, K_index, dt,...
+nPrev = round((nOut-1)/2);
+mid = nodes((end+1)/2);
+positions = [-nPrev:-nPrev+nOut-1]*dxOut + mid;
+nodeOut = round(positions/dx)+1;
+
+calculateERP(cores, pathToSave, mainElvira, project, cellType, K, K_index, dt,...
              step_save, pre_dur, pre_step, fun_sodium, h_index,...
-             j_index, Imax, Istep, CI_step, sigma_L, Cm, nodes)
+             j_index, Imax, Istep, CI_step, sigma_L, Cm, nodes,nodeOut)
 
 exit
