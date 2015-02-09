d9b620415fead8fdccacb5684b728f3b808052e7
Jesús Carro <chuscarro@gmail.com>
2015-02-09 14:06:11 +0100

    Bugs in brackets

--- source/createFileElements.m
+++ source/createFileElements.m
@@ -3,10 +3,10 @@ function createFileElements(pathToSave, nNodes)
 f=fopen([pathToSave '/data/file_elements.dat'],'w');
 
 fprintf(f,'!----------- FILE ELEMENTS ----------------------------\n');
-fprintf(f,[num2str(nNodes-1) ' 1\n');
-fprintf(f,[num2str(nNodes-1) ' HT1DL02\n');
+fprintf(f,[num2str(nNodes-1) ' 1\n']);
+fprintf(f,[num2str(nNodes-1) ' HT1DL02\n']);
 for i=1:nNodes-1
-  fprintf(f,['  ' num2str(i) ' 1 ' num2str(i) ' ' num2str(i+1) '\n'])
+  fprintf(f,['  ' num2str(i) ' 1 ' num2str(i) ' ' num2str(i+1) '\n']);
 end 
 fclose(f);
 
--- source/createFileNodes.m
+++ source/createFileNodes.m
@@ -6,7 +6,7 @@ fprintf(f,'!----------- FILE NODES -------------------------------\n');
 fprintf(f,[num2str(length(nodes)) ' 1 1\n']);
 fprintf(f,[num2str(length(nodes)) ' NODETYPE\n']);
 for i=1:length(nodes)
-  fprintf(f,['  ' num2str(i) ' 1 ' num2str(nodes(i)) '\n'])
+  fprintf(f,['  ' num2str(i) ' 1 ' num2str(nodes(i)) '\n']);
 end 
 fclose(f);
 
