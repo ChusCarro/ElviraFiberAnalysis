c03d1c24bc52c3507fbf92302da5f17ac4f4833e
Jesús Carro <chuscarro@gmail.com>
2015-02-09 13:28:06 +0100

    Changed path to save

--- source/createRunElv.m
+++ source/createRunElv.m
@@ -1,6 +1,6 @@
 function createRunElv(pathToSave,mainElvira)
 
-f = fopen([pathToSave '/base/runelv'],'w');
+f = fopen([pathToSave '/runelv'],'w');
 
 fprintf(f,'# $1 number of processes\n');
 fprintf(f,'# $2 input data file with full path\n');
@@ -10,5 +10,5 @@ fprintf(f,'# $3 output identifier with full path of output directory\n');
 %fprintf(f,['nohup mpirun -np $1 ' mainElvira ' -i $2 -o $3 </dev/null 2>&1\n']);
 fprintf(f,['mpirun -np $1 ' mainElvira ' -i $2 -o $3 </dev/null 2>&1\n']);
 fclose(f);
-fileattrib([pathToSave '/base/runelv'],'+x')
+fileattrib([pathToSave '/runelv'],'+x')
 
